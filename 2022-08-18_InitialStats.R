adb4c75cd9d62b80b33cfee8cba1fa10cb182c66
slcornett <slcornett@utexas.edu>
2022-08-18 11:33:29 -0500

    histogram made!

--- 2022-08-18_InitialStats.R
+++ 2022-08-18_InitialStats.R
@@ -6,7 +6,8 @@ library(ggplot2)
 f <- "https://raw.githubusercontent.com/slcornett/FoodDepBehaviorPilot/main/Summer22_FoodDepPilot_Fish_Dissection_Record.csv"
 d <- read_csv(f, col_names = TRUE)
 print(d)
-h1 <- ggplot(data = d)
-h1 <- h1 + geom_histogram(aes(x = MorphSex), stat = "count") + facet_wrap(~Condition)
-h1 <- h1 + ggtitle("Sex per Food Condition")
+h1 <- ggplot(data = d, aes(x = MorphSex, colour = MorphSex)) +
+  geom_histogram(stat = "count") +
+  facet_wrap(~Condition) +
+  ggtitle("Members of Each MorphSex per Food Condition")
 h1
