56d2c63b456e7a01223fb5ec3535968f0ef7192d
slcornett <slcornett@utexas.edu>
2023-02-09 10:01:43 -0600

    rename and added fish data to GnRHR2+GAPDH csv

--- FoodDepPilot_GeneExpression.R
+++ FoodDepPilot_GeneExpression.R
@@ -7,4 +7,7 @@ library(ggplot2)
 #library(ggsci) # science theme for ggplot2
 #library(cowplot)
 library(car) #anova
-# load dataset
+# load dataset----
+f2 <- "https://raw.githubusercontent.com/slcornett/FoodDepBehaviorPilot/main/fdp_datasets/2023-02-08_GnRHR2GAPDH.csv"
+df2 <- read_csv(f2, col_names = TRUE)
+print(df2)
