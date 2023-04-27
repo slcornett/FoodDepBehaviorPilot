1d1a248c0b71d77772f72e2d2ceed4433847ee46
slcornett <slcornett@utexas.edu>
2023-04-27 11:26:28 -0500

    graphs of read counts

--- 2023-04-27_sailfin_rnaseq_goi.R
+++ 2023-04-27_sailfin_rnaseq_goi.R
@@ -6,35 +6,79 @@ library(tidyverse)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(paletteer)
+library(patchwork)
 #library(ggsci) # science theme for ggplot2
 
 # load dataset----
 f3<- "https://raw.githubusercontent.com/slcornett/FoodDepBehaviorPilot/main/fdp_datasets/SailfinTPM_GOIs_14fish.csv"
 d3 <- read_csv(f3, col_names = TRUE)
 
-# graph read counts:
+# collapse columns----
+## use pivot_longer() to collapse days into single column, with Day ID as a new categorical data column
+RNA.reads <- d3 %>%
+  pivot_longer(cols=c('FPL1',
+                      'FPL10',
+                      'FPL11',
+                      'FPL12',
+                      'FPL13',
+                      'FPL17',
+                      'FPL18',
+                      'FPL19',
+                      'FPL2',
+                      'FPL3',
+                      'FPL4',
+                      'FPL5',
+                      'FPL8',
+                      'FPL9'),
+               names_to='subject',
+               values_to='read_count')
+RNA.reads
+## peptides only: gene_name column == gnrh1, gnrh2, or gnrh3
+RNA.reads.pep <- RNA.reads %>% filter(gene_name == "GnRH1" | # | = or
+                                        gene_name == "GnRH2"|
+                                        gene_name == "GnRH3")
+## receptors only: gene_name column == gnrhr(A), gnrhr(B), gnrhr(C), MC4R
+RNA.reads.receptor <- RNA.reads %>% filter(gene_name == "GnRHR(A)" | # | = or
+                                             gene_name == "GnRHR(B)"|
+                                             gene_name == "GnRHR(C)"|
+                                             gene_name == "MC4R")
+
+# graph read counts----
 ## peptides
-FvNF.GnRH1.p <- ggplot(data = ge , aes(x = MorphSex,
-                                       y = g1.GAP.deltaCt,
-                                       fill = MorphSex)) +
-  # food : green from sg_GrnYl (#08A47F) # No food: orange from plasma (#E78140)
-  # scale_fill_manual(values = c("#08A47F", "#E78140")) + #fill = food condition
-  scale_fill_manual(values = c("#392682", "#3F86BC", "#83DDE0")) +
+pep.p <- ggplot(data = RNA.reads.pep , aes(x = gene_name,
+                                       y = read_count,
+                                       fill = gene_name)) +
+  scale_fill_discrete(type = paletteer_d("ggthemes::excel_Green")) +
   geom_boxplot(outlier.shape = NA) +
-  geom_point(position=position_jitterdodge(), size = 3) +
-  geom_abline(aes(intercept = 0, slope = 0)) +
-  labs(title = "GnRH1",
-       x = "Morphological Sex",
-       y = "Relative Whole Brain Expression (GAPDH)") +
+  geom_point(position = position_jitterdodge(), size = 3) +
+  labs(x = "Gene of Interest",
+       y = "Reads") +
   theme_classic() +
-  scale_y_continuous(limits = c(-5.0,8.0),
-                     n.breaks = 12) + # breaks=pretty(sex.f$deltaCt, n=15)
   theme(plot.title = element_text(size = 28, color = "black"),
-        axis.title.x = element_text(size = 18, color = "black"), # x-axis
-        axis.text.x = element_text(size = 18, color = "black"),
-        axis.title.y = element_text(size = 18, color = "black"), # y-axis
+        axis.title.x = element_text(size = 14, color = "black", face = "bold"), # x-axis
+        axis.text.x = element_text(size = 14, color = "black", face = "italic"),
+        axis.title.y = element_text(size = 14, color = "black", face = "bold"), # y-axis
         axis.text.y = element_text(size = 14, color = "black"),
-        legend.position = "none") + #hide legend)
-  #facet_wrap(~MorphSex) # fill = food condition
-  facet_wrap(~FoodCondition) # fill = MorphSex
-FvNF.GnRH1.p
+        legend.position = "none") #hide legend)
+pep.p
+
+## receptors
+receptor.p <- ggplot(data = RNA.reads.receptor , aes(x = gene_name,
+                                                     y = read_count,
+                                                     fill = gene_name)) +
+  scale_fill_discrete(type = paletteer_d("ggthemes::excel_Blue")) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_point(position = position_jitterdodge(), size = 3) +
+  labs(x = "Gene of Interest",
+       y = "Reads") +
+  theme_classic() +
+  theme(plot.title = element_text(size = 28, color = "black"),
+        axis.title.x = element_text(size = 14, color = "black", face = "bold"), # x-axis
+        axis.text.x = element_text(size = 14, color = "black", face = "italic"),
+        axis.title.y = element_text(size = 14, color = "black", face = "bold"), # y-axis
+        axis.text.y = element_text(size = 14, color = "black"),
+        legend.position = "none") #hide legend)
+receptor.p
+
+pep.p + receptor.p
