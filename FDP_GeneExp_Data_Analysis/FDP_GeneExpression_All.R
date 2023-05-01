3ab3540e23e928ede1c0232e59f1401547811a89
slcornett <slcornett@utexas.edu>
2023-05-01 15:41:55 -0500

    gapdh avg ct graph added

--- FDP_GeneExp_Data_Analysis/FDP_GeneExpression_All.R
+++ FDP_GeneExp_Data_Analysis/FDP_GeneExpression_All.R
@@ -45,10 +45,35 @@ ge <- ge %>% mutate(g2.eB.deltaCt = (GnRH2_CtAvg - EarlyB_CtAvg), # same plate c
 #                     RA.deltaCt = GnRHRA_CtAvg - Control_AvgCt) # gnrhRA
 print(ge)
 # Food v NoFood Comparisons----
-## Gnrh1
+## Gapdh (housekeeping)
+FvNF.gapdh.p <- ggplot(data = ge , aes(x = MorphSex,
+                                       y = GAPDH_CtAvg,
+                                       fill = MorphSex)) +
+  # food : green from sg_GrnYl (#08A47F) # No food: orange from plasma (#E78140)
+  # scale_fill_manual(values = c("#08A47F", "#E78140")) + #fill = food condition
+  scale_fill_manual(values = c("#392682", "#3F86BC", "#83DDE0")) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_point(position=position_jitterdodge(), size = 3) +
+  labs(title = "GAPDH",
+       x = "Morphological Sex",
+       y = "Avg Whole Brain GAPDH Expression (Ct)") +
+  theme_classic() +
+  scale_y_continuous(limits = c(0.0,27.0),
+                      n.breaks = 14) + # breaks=pretty(sex.f$deltaCt, n=15)
+  theme(plot.title = element_text(size = 28, color = "black"),
+        axis.title.x = element_text(size = 12, color = "black", face = "bold"), # x-axis
+        axis.text.x = element_text(size = 12, color = "black"),
+        axis.title.y = element_text(size = 12, color = "black", face = "bold"), # y-axis
+        axis.text.y = element_text(size = 12, color = "black"),
+        legend.position = "none") + #hide legend)
+  #facet_wrap(~MorphSex) # fill = food condition
+  facet_wrap(~FoodCondition) # fill = MorphSex
+FvNF.gapdh.p
+
+## GnRH1
 FvNF.GnRH1.p <- ggplot(data = ge , aes(x = MorphSex,
-                                      y = g1.GAP.deltaCt,
-                                      fill = MorphSex)) +
+                                       y = g1.GAP.deltaCt,
+                                       fill = MorphSex)) +
   # food : green from sg_GrnYl (#08A47F) # No food: orange from plasma (#E78140)
   # scale_fill_manual(values = c("#08A47F", "#E78140")) + #fill = food condition
   scale_fill_manual(values = c("#392682", "#3F86BC", "#83DDE0")) +
@@ -67,7 +92,7 @@ FvNF.GnRH1.p <- ggplot(data = ge , aes(x = MorphSex,
         axis.title.y = element_text(size = 18, color = "black"), # y-axis
         axis.text.y = element_text(size = 14, color = "black"),
         legend.position = "none") + #hide legend)
-#facet_wrap(~MorphSex) # fill = food condition
+  #facet_wrap(~MorphSex) # fill = food condition
   facet_wrap(~FoodCondition) # fill = MorphSex
 FvNF.GnRH1.p
 
